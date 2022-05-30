194c9a04e15688f332ccb2f474fa5aa5db7007b4
Donggyu <a29661498@gmail.com>
2022-05-30 16:04:09 +0900

    feat: 회원가입 기능 구현

--- src/main/java/woowacourse/shoppingcart/controller/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/controller/CustomerController.java
@@ -1,4 +1,28 @@
 package woowacourse.shoppingcart.controller;
 
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import woowacourse.shoppingcart.dto.CustomerRequest;
+import woowacourse.shoppingcart.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody CustomerRequest request) {
+        customerService.save(request);
+
+        return ResponseEntity.created(URI.create("/login")).build();
+    }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -1,13 +1,30 @@
 package woowacourse.shoppingcart.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import woowacourse.shoppingcart.dto.CustomerRequest;
 
 @DisplayName("회원 관련 기능")
 public class CustomerAcceptanceTest extends AcceptanceTest {
-    @DisplayName("회원가입")
+
+    @DisplayName("email, password, username 을 통해서 회원가입을 할 수 있다.")
     @Test
     void addCustomer() {
+        // given
+        final CustomerRequest request =
+                new CustomerRequest("email@email.com", "password1!", "azpi");
+
+        // when
+        final ExtractableResponse<Response> response = AcceptanceFixture.post(request, "/api/customers");
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
     }
 
     @DisplayName("내 정보 조회")
