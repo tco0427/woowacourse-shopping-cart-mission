31e9c89d0c13b2523701b80a792ef4b2c98ce3c5
Donggyu <a29661498@gmail.com>
2022-06-01 11:23:25 +0900

    refactor: 사용하지 않는 DeleteException 제거

--- src/main/java/woowacourse/shoppingcart/exception/DeleteException.java
+++
@@ -1,7 +0,0 @@
-package woowacourse.shoppingcart.exception;
-
-public class DeleteException extends IllegalArgumentException {
-    public DeleteException(String message) {
-        super(message);
-    }
-}
