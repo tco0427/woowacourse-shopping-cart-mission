2861061ffdc31d48eed6532df18de4eff51e1b22
Donggyu <a29661498@gmail.com>
2022-06-07 11:59:39 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/woowacourse/shoppingcart/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package woowacourse.shoppingcart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("장바구니 API Document")
+                .version("1.0")
+                .build();
+    }
+}
