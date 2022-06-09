52eefa56952f503f2655fe2b83ac34e88dac23d7
Donggyu <a29661498@gmail.com>
2022-06-09 10:28:16 +0900

    feat: data.sql 수량 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/apple-watch-1.jpg','Apple Watch Series 5');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Apple Watch Series 5', 120, 5, 1);
+values ('Apple Watch Series 5', 120, 10, 1);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/apple-watch-nike.jpg','Apple Watch Nike');
@@ -14,91 +14,91 @@ insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/apple-watch-aluminum.jpg','Apple watch Aluminum');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Apple watch Aluminum', 79, 5, 3);
+values ('Apple watch Aluminum', 79, 8, 3);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/apple-smart-watch.jpg','Apple Smart Watch');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Apple Smart Watch', 35, 0, 4);
+values ('Apple Smart Watch', 35, 20, 4);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/microsoft-surface-book.jpg','Microsoft Surface Book');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Microsoft Surface Book', 550, 5, 5);
+values ('Microsoft Surface Book', 550, 8, 5);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/mackbook-pro-retina.jpeg','Mackbook Pro Letina');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Mackbook Pro Letina', 250, 5, 6);
+values ('Mackbook Pro Letina', 250, 10, 6);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/apple-imac.jpg','Apple iMac');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Apple iMac', 300, 5, 7);
+values ('Apple iMac', 300, 10, 7);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/xbox-wireless-controller.jpg','XBox Wireless Controller');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('XBox Wireless Controller', 180, 0, 8);
+values ('XBox Wireless Controller', 180, 10, 8);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/XBox-One-X-1TB.jpg','XBox One X 1TB');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('XBox One X 1TB', 100, 5, 9);
+values ('XBox One X 1TB', 100, 15, 9);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/playstation-4.jpeg','Playstation 4');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Playstation 4', 100, 5, 10);
+values ('Playstation 4', 100, 15, 10);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Controller-for-Playstation-4.jpg','Controller for PlayStation 4');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Controller for PlayStation 4', 200, 5, 11);
+values ('Controller for PlayStation 4', 200, 15, 11);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/wireless-playstation.jpg','Wireless Playstation');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Wireless Playstation', 150, 5, 12);
+values ('Wireless Playstation', 150, 15, 12);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Sony-Series-Headphones.jpg','Sony Series Headphones');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Sony Series Headphones', 100, 5, 13);
+values ('Sony Series Headphones', 100, 15, 13);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Bluetooth-Headphones.jpg','Bluetooth Headphones');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Bluetooth Headphones', 100, 5, 14);
+values ('Bluetooth Headphones', 100, 15, 14);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Jabra-Wireless-Stereo-Headset.jpg','Jabra Wireless Stereo Headset');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Jabra Wireless Stereo Headset', 80, 5, 15);
+values ('Jabra Wireless Stereo Headset', 80, 15, 15);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Beats-On-Ear-Headphone.jpg','Beats On-Ear Headphone');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Beats On-Ear Headphone', 100, 5, 16);
+values ('Beats On-Ear Headphone', 100, 15, 16);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Digital-Alarm-Clock.jpg','Digital Alarm Clock');
 
 insert into product (name, price, stock_quantity, image_id)
-values ('Digital Alarm Clock', 30, 5, 17);
+values ('Digital Alarm Clock', 30, 50, 17);
 
 insert into image (image_url, image_alt)
 VALUES ('https://develoger.kr/wp-content/uploads/Mkay-Bluetooth-Headphones.jpg','Mkay Bluetooth Headphones');
